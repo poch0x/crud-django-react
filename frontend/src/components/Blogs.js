5ae7fae38703ab34fe5630cdb455e82329dbb75f
Jorge Infante <jor22.n@gmail.com>
2023-01-31 12:44:28 -0300

    add hundleUpdate and hundleDelete

--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -4,57 +4,112 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Modal from 'react-bootstrap/Modal';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
-import ModalHeader from 'react-bootstrap/ModalHeader';
-import ModalFooter from 'react-bootstrap/ModalFooter';
+import ModalHeader from 'react-bootstrap/esm/ModalHeader';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 import axios from 'axios';
 
 
-export default function Blogs ({ blogs = [], setBlogs }) {
-    
+export default function Blogs({ blogs = [], setBlogs }) {
+
+    const [show, setShow] = useState(false);
+    const [record, setRecord] = useState(null);
+
+    const handleUpdate = async (id, value) => {
+        return axios.put(`/put/${id}/`, value)
+        .then((reponse) => {
+            const { data } = reponse;
+            const newBlogs = blogs.map (blog => {
+                if (blog.id === id) {
+                    return data;
+                }
+                return blog;
+            })
+            setBlogs(newBlogs)
+        }).catch(() => {
+            alert('Algo fue mal!')
+        })
+    }
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
+    const handleSaveChanges = async () => {
+        await handleUpdate(record.id, {body: record.body});
+        handleClose();
+    }
+
+    const handleChange = (e) => {
+        setRecord({
+            ...record,
+            body: e.target.value
+        })
+    }
+
+    const handleDelete = (id) => {
+        axios.delete(`/delete/${id}`)
+        .then(() => {
+            const newBlogs = blogs.filter(blog => {
+                return blog.id !== id
+            });
+            setBlogs(newBlogs)
+        }).catch(() => {
+            alert('Algo fue mal!')
+        })
+    }
+
+
+
     return (
         <div>
-            <ListGroup>
-                {blogs.map( blog => {
-                    return (
-                <ListGroupItem key={blog.id} className='d-flex justify-content-between aligth-items-center'>
-                    
-                    {blog.body}
-                    <div>
-                        <FaEdit size={20} style={{cursor: 'pointer'}}/>
-                        <FaTrashAlt size={20} style={{cursor: 'pointer'}}/>
-                    </div>
+        <ListGroup>
+            {blogs.map( blog => {
+                return (
+            <ListGroupItem key={blog.id} className='d-flex justify-content-between align-items-center'>
 
-                </ListGroupItem>
-                    )
-                })}
-            </ListGroup>
+                {blog.body}
+                <div>
+                <FaEdit
+                onClick={() => {setRecord(blog); setShow(true)}}
+                size={20} style={{cursor: 'pointer'}}/>
+                    &nbsp;
+                <FaTrashAlt
+                onClick={() => { handleDelete (blog.id) }}
+                size={20} style={{cursor: 'pointer'}}/>
+                </div>
 
-            <Modal>
+            </ListGroupItem>
+            )
+            })}
+        </ListGroup>
 
-                <ModalHeader>
-                    <Modal.Title>
-                        EDIT BLOG
-                    </Modal.Title>
-                </ModalHeader>
 
-                <Modal.Body>
-                    <FormControl/>
-                </Modal.Body>
+        <Modal show={show} onHide={handleClose}>
 
-                <ModalFooter>
-                    <Button variant='dark'>
-                        CERRAR
-                    </Button>
+            <ModalHeader closeButton>
+                <Modal.Title>
+                    EDIT BLOG
+                </Modal.Title>
+            </ModalHeader>
 
-                    <Button variant='dark'>
-                        SAVE
-                    </Button>
-                </ModalFooter>
+            <Modal.Body>
+                <FormControl 
+                value={record ? record.body : ''}
+                onChange={handleChange}
+                />
+            </Modal.Body>
 
-            </Modal>
+            <Modal.Footer>
+                <Button variant='dark' onClick={handleClose}>
+                    CERRAR
+                </Button>
+
+                <Button variant='dark' onClick={handleSaveChanges}>
+                    SAVE
+                </Button>
+            </Modal.Footer>
+
+        </Modal>
         </div>
-
     )
 }
-
